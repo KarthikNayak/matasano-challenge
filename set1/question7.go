0098800478c9d02696398f9b47201576c7be7423
karthik nayak <karthik.188@gmail.com>
2018-06-13 10:42:38 +0530

    Set 1: Question 7
    Signed-off-by: karthik nayak <karthik.188@gmail.com>

---
+++ set1/question7.go
@@ -0,0 +1,36 @@
+package set1
+
+import (
+	"crypto/aes"
+	"fmt"
+	"io/ioutil"
+	"log"
+
+	"gitlab.com/karthiknayak/matasano/types"
+)
+
+func SolveQ7(fileName string) (string, error) {
+	content, err := ioutil.ReadFile(fileName)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	b64 := types.Base64{ B: content}
+	data, err := b64.Decode()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	block, err := aes.NewCipher([]byte("YELLOW SUBMARINE"))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for i := 0; i < len(data); i += 16 {
+		block.Decrypt([]byte(data[i:i+16]), []byte(data[i:i+16]))
+	}
+
+	fmt.Println(string(data))
+
+	return "", nil
+}
---
+++ set1/question7_test.go
@@ -0,0 +1,10 @@
+package set1
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestSolveQ7(t *testing.T) {
+	fmt.Println(SolveQ7("question7_data"))
+}
